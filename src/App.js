a874edde6a0c509f1e301083055865cfc7f4dd33
losajjang <losajjang@naver.com>
2022-03-28 10:38:53 +0900

    implemenration of Detail Page function

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { Navbar, Container, NavDropdown, Nav, Button } from "react-bootstrap";
 import "./App.css";
 import Data from "./data";
+import { Link, Route, Switch } from "react-router-dom";
+import Detail from "./Detail";
 
 function App() {
   let [shoes, shoes변경] = useState(Data);
@@ -14,8 +16,8 @@ function App() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/detail/:id">Detail</Nav.Link>
               <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -27,21 +29,23 @@ function App() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-
-      <div className="Jumbotron">
-        <h1>20% Season Off</h1>
-        <p>
-          This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content
-          or information.
-        </p>
-        <p>
-          <Button variant="primary">Learn more</Button>
-        </p>
-      </div>
-      <div className="container">
-        <div className="row">
-          {/* ↓아래 주석은 내가 한 것. 하드코딩이라 보기 좋지 않음. */}
-          {/* {shoes.map(function (글, i) {
+      <Switch>
+        <Route path="/" exact>
+          <div className="Jumbotron">
+            <h1>20% Season Off</h1>
+            <p>
+              This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured
+              content or information.
+            </p>
+            <p>
+              <Button variant="primary">Learn more</Button>
+            </p>
+          </div>
+          {/* Jumbotron 끝 */}
+          <div className="container">
+            <div className="row">
+              {/* ↓아래 주석은 내가 한 것. 하드코딩이라 보기 좋지 않음. */}
+              {/* {shoes.map(function (글, i) {
             return (
               <div className="col-md-4">
                 <img src="https://codingapple1.github.io/shop/shoes[i].jpg" width="100%" alt="" />
@@ -52,21 +56,26 @@ function App() {
               </div>
             );
           })} */}
-          {/* ↑여기까지가 내가 한 것. */}
+              {/* ↑여기까지가 내가 한 것. */}
 
-          {shoes.map((t, i) => {
-            // 반복문을 만들 때는 key={i}를 반드시 생성하자.
-            return <Card shoes={shoes[i]} i={i} key={i}/>;
-          })}
-        </div>
-      </div>
+              {shoes.map((t, i) => {
+                // 반복문을 만들 때는 key={i}를 반드시 생성하자.
+                return <Card shoes={shoes[i]} i={i} key={i} />;
+              })}
+            </div>
+          </div>
+        </Route>
+        <Route path="/detail/:id" exact>
+          <Detail shoes={shoes}></Detail>
+        </Route>
+      </Switch>
     </div>
   );
 
   function Card(props) {
     return (
       <div className="col-md-4">
-        <img src={`https://codingapple1.github.io/shop/shoes${props.i+1}.jpg`} width="100%" alt="" />
+        <img src={`https://codingapple1.github.io/shop/shoes${props.i + 1}.jpg`} width="100%" alt="" />
         <h4>{props.shoes.title}</h4>
         <p>
           {props.shoes.content} & {props.shoes.price}
---
+++ src/Detail.js
@@ -0,0 +1,68 @@
+/* eslint-disable*/
+
+import React, { useEffect, useState } from "react";
+import { useHistory, useParams } from "react-router-dom";
+import styled from "styled-components";
+import "./Detail.scss";
+
+let 박스 = styled.div`
+  padding: 20px;
+`;
+
+let 제목 = styled.h4`
+  font-size: 25px;
+  color: ${(props) => props.색상};
+`;
+
+function Detail(props) {
+  let [재고경고, 재고경고변경] = useState(true);
+  useEffect(() => {
+    // let 타이머 = setTimeout(() => {
+    //   return <Rer></Rer>;
+    // }, 2000);
+  });
+
+  let history = useHistory();
+  let { id } = useParams();
+  let 찾은상품 = props.shoes.find(function (상품) {
+    return 상품.id == id;
+  });
+
+  function 재고경고문() {
+    return (
+      <div id="rer" className="my-alert2">
+        <p>재고가 얼마 남지 않았습니다</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="container">
+      <박스>
+        <제목 className="red">Detail</제목>
+        <재고경고문 />
+      </박스>
+      <div className="row">
+        <div className="col-md-6">
+          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+        </div>
+        <div className="col-md-6 mt-4">
+          <h4 className="pt-5">{찾은상품.title}</h4>
+          <p>{찾은상품.content}</p>
+          <p>{찾은상품.price}원</p>
+          <button className="btn btn-danger">주문하기</button>
+          <button
+            className="btn btn-danger"
+            onClick={() => {
+              history.goBack();
+            }}
+          >
+            뒤로가기
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Detail;
